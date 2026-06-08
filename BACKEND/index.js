31e0687f95b15ef23c289e61ce8e7a0ca8f5baaf
johnsimon-007@gmail.com <johnsimon-007>
2026-06-08 20:06:49 +0530

    made some changes

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -39,7 +39,10 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
